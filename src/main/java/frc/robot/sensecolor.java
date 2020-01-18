b12e4dfed0df47c1cf7406b128863eecdbec7bd4
minevsr0bl0x <57153112+minevsr0bl0x@users.noreply.github.com>
2020-01-18 09:32:28 -0500

    Update sensecolor.java

--- src/main/java/frc/robot/sensecolor.java
+++ src/main/java/frc/robot/sensecolor.java
@@ -9,12 +9,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import edu.wpi.first.wpilibj.DriverStation;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.Locale;
 public class gear ratio extends subsystems 
     // if uisng wheel 1/4 the size of color wheel
+    
+    public static void {
+    String 'B' = blue;
+    String 'G' = green;
+    String 'R' = red;
+    String 'Y' = yellow;
+    }
     
     public static void {
     telemetry.addData ("red" sensorColor.red());
@@ -24,52 +32,52 @@ public class gear ratio extends subsystems
     }
 
 public static void main(String []args) {
-if (sensorColor.red() && insert blue) {
+if (sensorColor.red() && String blue) {
     do nothing;
 }
-else if (sensorColor.red() && insert yellow) {
+else if (sensorColor.red() && String yellow) {
     move 180 degrees  clockwise 1/2 rotation;
 }
-else if (sensorColor.red() && insert green) {
+else if (sensorColor.red() && String green) {
     move 180 degrees counterclockwise 1/2 rotation;
 }
-else if (sensorColor.red() && insert red) {
+else if (sensorColor.red() && String red) {
     move 360 degrees clockwise 1 rotation;
 }
-else if (sensorColor.blue() && insert red) {
+else if (sensorColor.blue() && String red) {
     do nothing;
 }
-else if (sensorColor.blue() && insert green) {
+else if (sensorColor.blue() && String green) {
     move 180 degrees  clockwise 1/2 rotation;
 }
-else if (sensorColor.blue() && insert yellow) {
+else if (sensorColor.blue() && String yellow) {
     move 180 degrees counterclockwise 1/2 rotation;
 }
-else if (sensorColor.blue() && insert blue) {
-    move 360 degrees clockwise 1 rotation;
-} 
-else if (sensorColor.green() && insert yellow) {
-    do nothing;
-}
-else if (sensorColor.green() && insert red) {
-    move 180 degrees  clockwise 1/2 rotation;
-}
-else if (sensorColor.green() && insert blue) {
-    move 180 degrees counterclockwise 1/2 rotation;
-}
-else if (sensorColor.green() && insert green) {
+else if (sensorColor.blue() && String blue) {
     move 360 degrees clockwise 1 rotation;
 } 
-else if (sensorColor.yellow() && insert green) {
+else if (sensorColor.green() && String yellow) {
     do nothing;
 }
-else if (sensorColor.yellow() && insert blue) {
+else if (sensorColor.green() && String red) {
     move 180 degrees  clockwise 1/2 rotation;
 }
-else if (sensorColor.yellow() && insert red) {
+else if (sensorColor.green() && String blue) {
     move 180 degrees counterclockwise 1/2 rotation;
 }
-else if (sensorColor.yellow() && insert yellow) {
+else if (sensorColor.green() && String green) {
+    move 360 degrees clockwise 1 rotation;
+} 
+else if (sensorColor.yellow() && String green) {
+    do nothing;
+}
+else if (sensorColor.yellow() && String blue) {
+    move 180 degrees  clockwise 1/2 rotation;
+}
+else if (sensorColor.yellow() && String red) {
+    move 180 degrees counterclockwise 1/2 rotation;
+}
+else if (sensorColor.yellow() && String yellow) {
     move 360 degrees clockwise 1 rotation;
 } 
 else (nothing) {
